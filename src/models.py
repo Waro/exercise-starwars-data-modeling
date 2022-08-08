32ee0d0beef6addb2eda8192d052617a0d2c0fa6
Mario <waro.brambilla@gmail.com>
2022-08-08 18:31:41 +0000

    did it

--- src/models.py
+++ src/models.py
@@ -8,23 +8,60 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class user(Base):
+    __tablename__ = 'user'
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=True)
+    last_name = Column(String(250), nullable=True)
+    nickname = Column(String(250), nullable=False)
+
+
+class character(Base):
+    __tablename__ = 'character'
+  
+    id = Column(Integer, primary_key=True)
+    password = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    affiliation = Column(String(250), nullable=True)
+    light_saber = Column(String(250), nullable=True)
+
+class planet(Base):
+    __tablename__ = 'planet'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    surface = Column(Integer, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class starship(Base):
+    __tablename__ = 'starship'
+
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    guns = Column(String(250), nullable=False)
+    size = Column(Integer, nullable=False)
+    speed = Column(Integer, nullable=False)
+    affiliation = Column(String(250), nullable=True)
+
+class faction(Base):
+    __tablename__ = 'faction'
+    
+    id = Column(Integer,primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class favourite(Base):
+    __tablename__='favourite'
+
+    id = Column(Integer,primary_key=True)
+    user = Column(Integer, ForeignKey('user.id'))
+    character = Column(Integer, ForeignKey('character.id'))
+    planet = Column(Integer, ForeignKey('planet.id'))
+    starship = Column(Integer, ForeignKey('starship.id'))
+    affiliation = Column(Integer, ForeignKey('faction.id'))
+
+  
 
     def to_dict(self):
         return {}
